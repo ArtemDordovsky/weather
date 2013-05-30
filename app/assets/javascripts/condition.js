16b89e6dc3046a4a8c496fc41b0b6ee54ea52493
Artem Dordovsky <DordovskyArtem@gmail.com>
2013-05-30 19:30:53 +0300

    small fixed

--- app/assets/javascripts/condition.js
+++ app/assets/javascripts/condition.js
@@ -1,25 +1,24 @@
 $(function(){
     $(".graph").peity("line", {width: 300, height: 150});
-    var days = ""
-    var date = ""
-    $("body").on('click', '#three_days', function(){
-        var forecast = $("#forecast_three_days")
-        forecast.toggle();
-          days = 3
-          $.ajax({
-            type: "GET",
-            url: "/forecasts",
-            data: {num_of_days: days}
-          }).done(function( html ) {
-            $("#forecast_three_days").(html);});
+    $('body').on('click', '#three_days', function(){
+      var days = 3;
+      $.ajax({
+        type: "GET",
+        url: "/forecasts",
+        data: {num_of_days: days}
+      }).done(function( html ) {
+        $("#forecast_three_days").html(html);});
     });
-    $("body").on('click', 'button', function(){
-        date = "2013-06-08"
-    $.ajax({
+    $('body').on('click', 'button', function(){
+      var year = $('select#date_year option:selected').val();
+      var month = $('select#date_month option:selected').val();
+      var day = $('select#date_day option:selected').val();
+      var date = year+'-'+month+'-'+day;
+      $.ajax({
         type: "GET",
         url: "/forecasts",
         data: {date: date}
-    }).done(function( html ) {
-      $("#forecast_of_date").escape(html);});
+      }).done(function( html ) {
+        $("#forecast_of_date").html(html);});
     });
 });
--- app/controllers/forecasts_controller.rb
+++ app/controllers/forecasts_controller.rb
@@ -1,5 +1,6 @@
 class ForecastsController < ApplicationController
   def show
     @forecast = WeatherCreator.new('donetsk','ukraine',params[:num_of_days], params[:date]).forecast
+    render 'show', layout: false
   end
 end
